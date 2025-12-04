f35feae99fba5c0cedf98d4bb261176c5f62d4e0
minhtri <minhvutri12@gmail.com>
2025-12-04 08:07:29 +0700

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products/all',
+  },
+  {
+    path: 'products/:category',
+    loadComponent: () => import('./pages/products-grid/products-grid'),
+  },
+  {
+    path: 'wishlist',
+    loadComponent: () => import('./pages/my-wishlist/my-wishlist'),
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./pages/products-grid/products-grid'),
+  },
+];
